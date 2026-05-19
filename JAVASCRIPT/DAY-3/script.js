e1b041e516a6083c6a5da32e678cac3fa5d65d0f
Yaswanth <yaswanthyassu9726@gmail.com>
2026-05-19 11:37:46 +0530

    Added remaining files

---
+++ JAVASCRIPT/DAY-3/script.js
@@ -0,0 +1,38 @@
+let a = 10;
+let b = 20;
+let op = '+'
+switch (op){
+    case '+':
+        console.log(a + b);
+        break;
+    case '-':
+        console.log(a - b);
+        break
+}
+
+
+
+
+
+function myFun(num1, num2){
+    return num1 * num2
+}
+
+function myFun(num1, num2){
+    return num1 ** num2
+
+}
+console.log(myFun(2,2));
+
+
+
+if (a === b){
+    console.log("Yaswanth");
+}else{
+    console.log("Rami"); 
+}
+
+
+const fun = (a,b) => a+b
+
+console.log(fun(1,1));
---
+++ JAVASCRIPT/DAY-5/script.js
@@ -0,0 +1,23 @@
+// console.log(document.body);
+// let para = document.getElementsByTagName("p")
+// console.log(para[2]);
+
+// let heading = document.getElementsByTagName("h1")[0]
+// console.log(heading);
+
+
+// let buchi = document.getElementsByClassName("buchi")
+// console.log(buchi[0]);
+
+// let boo = document.getElementById("boo")
+// boo.innerHTML = "<u>hello</u>"
+// console.log(boo.innerHTML);
+
+let paras = document.getElementsByTagName("p")
+paras[4].innerHTML = "Yaswanth"
+
+document.getElementsByClassName("para")[1].innerHTML = "Welcome to JS"
+
+
+
+
