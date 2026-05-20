cbe4d9d04b76d66bb1b8ab0810ba51b365778fef
Yaswanth <yaswanthyassu9726@gmail.com>
2026-05-20 11:19:47 +0530

    Fix DAY-4 submodule to normal folder

---
+++ JAVASCRIPT/DAY-4/script.js
@@ -0,0 +1,12 @@
+let arr = [10, 10.5, "Hello", true];
+console.log(arr);
+arr.push("her");
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(123);
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.splice(2, 1)
+console.log(arr);
